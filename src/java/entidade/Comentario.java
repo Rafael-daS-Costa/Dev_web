ae03eb54753498e55b7256590a2371d09ee3cbcf
Roberto Martins <robertomartins@id.uff.br>
2024-06-18 21:26:21 -0300

    Initial commit

---
+++ src/java/entidade/Comentario.java
@@ -0,0 +1,83 @@
+package entidade;
+
+
+public class Comentario {
+    private int id;
+    private String comentario;
+    private String data;
+    private String nomeUsuario;
+    private String categoria;
+    private int idusuario;
+    private int idcategoria;
+    
+
+    public Comentario() {
+    }
+    public Comentario(int id, String comentario, String data, String nomeUsuario, String categoria) {
+        this.id = id;
+        this.comentario = comentario;
+        this.data = data;
+        this.nomeUsuario = nomeUsuario;
+        this.categoria = categoria;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getNomeUsuario() {
+        return nomeUsuario;
+    }
+
+    public void setNomeUsuario(String nomeUsuario) {
+        this.nomeUsuario = nomeUsuario;
+    }
+
+    public int getIdusuario() {
+        return idusuario;
+    }
+
+    public void setIdusuario(int idusuario) {
+        this.idusuario = idusuario;
+    }
+
+    public int getIdcategoria() {
+        return idcategoria;
+    }
+
+    public void setIdcategoria(int idcategoria) {
+        this.idcategoria = idcategoria;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+    
+    
+
+    
+}
